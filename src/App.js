cc5042794713479722fc7664cc40c3ceee5a8928
juanfstillo <juan.stillo1@gmail.com>
2021-07-12 15:50:45 -0300

    Menu para la clase 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import React, { Component }  from 'react';
+import NavBar  from './components/NavBar';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+const HolaApp=()=>{
+  let nombre= 'pepe';
+
+  return(<p>Hola</p>)
+}
 function App() {
+  let nombre= 'pepe';
+  const style={backgroundColor:'blue'}
+  const saludar=()=>{
+    alert('hola');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <NavBar/> 
+     </div>
   );
 }
 
