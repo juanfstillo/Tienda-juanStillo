8c6f429713d5a8a6a8042724e449eb524dc8823e
juanfstillo <juan.stillo1@gmail.com>
2021-07-28 14:57:09 -0300

    Entrega 7 corregida

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import Item from './Item'
+import Item from'./Item'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
 
-function ItemList ( { itemList } ) 
+
+
+
+
+function ItemDetail ( { itemList } ) 
 {    return (
-        <div>
-            {itemList.filter( item => <Item item={item.id===1} />) }
-        </div>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={itemList[0]?.img} />
+    <Card.Body>
+      <Card.Title>{itemList[0]?.name} {itemList[0]?.characteristic}</Card.Title>
+      <Card.Text>
+      {itemList[0]?.description}
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroupItem>$ {itemList[0]?.price}</ListGroupItem>
+      <Button variant="warning">Comprar</Button>
+    </ListGroup>
+    {/* <Card.Body>
+      <Card.Link href="#">Card Link</Card.Link>
+      <Card.Link href="#">Another Link</Card.Link>
+    </Card.Body> */}
+  </Card>   
+ 
+        // <div>
+        //     <div >
+        //         <h4>{itemList[0]?.name}</h4>
+        //         <img src={itemList[0]?.img} alt={itemList[0]?.name} />
+        //     </div>
+        // </div>
     )
 }
-
-export default ItemList
+export default ItemDetail
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import ItemCount from './ItemCount'
 import ItemList from './ItemList'
+import ItemDetail from './ItemDetail'
 
 
 function ItemDetailContainer() {
@@ -12,60 +13,70 @@ function ItemDetailContainer() {
                 "name": "Kirschbaum",
                 "img": 'https://img.tennis-warehouse.com/cache/100/PL2130BK-1.jpg',
                 "characteristic": "Pro Line 2",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":1400
               }, {
                 "id": 2,
                 "name": "Kirschbaum",
                 "img": 'https://img.tennis-warehouse.com/cache/100/KFLASH-BK-1.jpg',
                 "characteristic": "Max Power",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":1600
               }, {
                 "id": 3,
                 "name": "Kirschbaum",
                 "img": 'https://img.tennis-warehouse.com/cache/100/PLE2125BLU-1.jpg',
                 "characteristic": "Super Smash Orange",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":1100
               }, {
                 "id": 4,
                 "name": "Zons",
                 "img": 'https://img.tennis-warehouse.com/cache/100/ISOCC16-1.jpg',
                 "characteristic": "Polymo Tour",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":900
               }, {
                 "id": 5,
                 "name": "Zons",
                 "img": 'https://img.tennis-warehouse.com/cache/100/ISOCC16-1.jpg',
                 "characteristic": "Polymo Xplosion",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":900
               }, {
                 "id": 6,
                 "name": "Luxilon",
                 "img": 'https://img.tennis-warehouse.com/cache/100/APRGS16-1.jpg',
                 "characteristic": "Alu Power",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":1900
               }, {
                 "id": 7,
                 "name": "Luxilon",
                 "img": 'https://img.tennis-warehouse.com/cache/100/APRGS16-1.jpg',
                 "characteristic": "Original",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":1900
               }, {
                 "id": 8,
                 "name": "Yonex",
                 "img": 'https://img.tennis-warehouse.com/cache/100/YPTR16L-1.jpg',
                 "characteristic": " Poly Tour",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":1000
               }, {
                 "id": 9,
                 "name": "Yonex",
                 "img": 'https://img.tennis-warehouse.com/cache/100/YPTS16BK-1.jpg',
                 "characteristic": " Poly Tour Drive",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":1100
               }, {
                 "id": 10,
                 "name": "Head",
                 "img": 'https://img.tennis-warehouse.com/cache/100/VMLT17-BK-1.jpg',
                 "characteristic": "Master",
+                "description":"This string has great durability and comes pre-stretched for better tension maintenance. Ideal for intermediate and advanced players with long, fast strokes.",
                 "price":700
               }] 
     
@@ -93,15 +104,20 @@ function ItemDetailContainer() {
         
     },[])
 
-    console.log(itemList)
     return (
-        <div >
-        <h1>Soy Item Detail Container</h1>
-            <ul>
-            <ItemList itemList={itemList} />
-          </ul>
 
-        </div>
+      <div >
+
+      <h1> Item Detail Container</h1>
+
+          <ul>
+
+          <ItemDetail itemList={itemList} />
+        </ul>
+
+
+
+      </div>
     )
 }
 
